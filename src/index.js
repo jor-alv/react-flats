596fbaf8fce95b1b52b6c3cd057e2fc70543269a
Jorge Alvarez <jorge.a.alvarez@protonmail.com>
2022-07-06 02:22:21 -0400

    Add mapbox-gl, plus markers

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import 'mapbox-gl/dist/mapbox-gl.css';
 import './assets/application.scss';
 import App from './components/app';
 import reportWebVitals from './reportWebVitals';
